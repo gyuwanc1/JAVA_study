054f314555eb69b5f057550f0d6a9ef5429c74ab
b_gyuwan <gyuwanc@gmail.com>
2023-05-02 15:51:11 +0900

    chapter_ex01
    ex01_ Shortcuts key

---
+++ chapter_01/basicsyntax/src/sec01_basicsyntax/EX01/Javabasicstructure.java
@@ -0,0 +1,23 @@
+package sec01_basicsyntax.EX01;
+/*
+처음 만든 클래스
+(여러 줄 주석 )
+*/
+
+/*여러줄주석*/ //<== CTL+Shift+/
+
+public class Javabasicstructure {
+	
+	public static void main(String[] args) {
+		
+	}	
+	
+	/*
+	 * public static void main(String[] args) {   <== main+ctrl+space
+	 * 
+	 * // 한줄주석 : 화면 출력 코드 = ctrl+/ System.out.println("콘솔 화면에 출력"); //
+	 * sysout+ctrl+스페이스바	 * 
+	 * 	 * 
+	 * }
+	 */
+}
