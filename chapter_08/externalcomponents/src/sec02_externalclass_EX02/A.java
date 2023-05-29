27b27cae794445506cff608a07026d3fd425b1db
b_gyuwan <gyuwanc@gmail.com>
2023-05-29 15:48:27 +0900

    chapter8
    package && import

---
+++ chapter_08/externalcomponents/src/sec02_externalclass_EX02/A.java
@@ -0,0 +1,11 @@
+package sec02_externalclass_EX02;
+
+public class A {
+	int m =3;
+	int n =4;
+	
+	void print() {
+		System.out.println(m + " , "+n);
+	}
+
+}
---
+++ chapter_08/externalcomponents/src/sec02_externalclass_EX02/ExternalClass_2.java
@@ -0,0 +1,10 @@
+package sec02_externalclass_EX02;
+
+public class ExternalClass_2 {
+	public static void main(String[] args) {
+		A a = new A();
+		a.print();// 3,4
+		
+	}
+
+}
