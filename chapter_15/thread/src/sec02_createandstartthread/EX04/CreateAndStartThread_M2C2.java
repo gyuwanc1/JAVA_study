ee09d13039e8d94d4d278540c157a8c1b732a0df
b_gyuwan <gyuwanc@gmail.com>
2023-10-25 16:49:41 +0900

    chapter15
    CreateAndStartThread && Runnable

---
+++ chapter_15/thread/src/sec02_createandstartthread/EX04/CreateAndStartThread_M2C2.java
@@ -0,0 +1,63 @@
+package sec02_createandstartthread.EX04;
+
+
+
+//1. 방법2(M2). Runnable 인터페이스 상속하여 클래스 생성 case2: 3개의 쓰레드 생성
+
+class SMIFileRunnable implements Runnable{
+	
+	
+	public void run() {
+		//2. 자막 번호 하나~다섯
+		String[] strArray = {"하나","둘","셋","넷","다섯"};
+		try {Thread.sleep(200);	} catch (InterruptedException e) {}
+		
+		//4. 자막번호 출력
+		for( int i =0; i<strArray.length; i++) {
+			System.out.println(" - (자막번호)"+strArray[i]);
+			try {Thread.sleep(500);	} catch (InterruptedException e) {}
+		}
+		
+	}
+
+}
+
+class VideoFileRunnable implements Runnable {
+	
+	public void run() {
+		//1. 비디오프레임 1~5
+		int[] intArray = {1,2,3,4,5};
+
+		
+		//3. 비디오 프레임 출력
+		for( int i =0; i<intArray.length; i++) {
+			System.out.print("(비디오프레임)"+intArray[i]);
+			try {Thread.sleep(500);	} catch (InterruptedException e) {}
+		}
+
+	}
+}
+
+
+
+public class CreateAndStartThread_M2C2 {
+	public static void main(String[] args) {
+		
+		//SMIFileRunnable 객체생성
+		Runnable smiFileRunnable = new SMIFileRunnable();
+		//smiFileRunnable.start();//오류
+		Thread thread1 = new Thread(smiFileRunnable);
+		thread1.start();
+		
+		//VideoFileRunnable 객체생성
+		Runnable videoFileRunnable = new VideoFileRunnable();
+		//smiFileRunnable.start();//오류
+		Thread thread2 = new Thread(videoFileRunnable);
+		thread2.start();
+		
+
+		
+	}
+
+
+}
